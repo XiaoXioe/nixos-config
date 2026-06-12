369f1978b87800e6ddf5c8b8f6f8bb5ac5def102
Klein Moretti (admin) <169626976+XiaoXioe@users.noreply.github.com>
2026-06-12 16:24:36 +0700

    refactor(core): move graphics to core and use mkNestedEnable

---
+++ modules/core/graphics.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  lib,
+  selfLib,
+  ...
+}:
+let
+  cfg = config.my.core.graphics;
+in
+{
+  options = selfLib.mkNestedEnable "core.graphics";
+
+  config = lib.mkIf cfg.enable {
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-vaapi-driver
+        libvdpau-va-gl
+        # vulkan
+
+        vulkan-loader
+        vulkan-tools
+
+        intel-media-driver # backup + fitur tambahan
+      ];
+    };
+
+    environment.systemPackages = with pkgs; [
+      libva-utils
+    ];
+
+    services.xserver.videoDrivers = [ "modesetting" ];
+
+  };
+}
