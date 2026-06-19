e381317426f2685d0cef35abad1fc51b2a2eb3a6
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-06-19 12:48:19 +0700

    chore: add hermes-agent to flake inputs

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # custompkgs = {
-    #   url = "path:/home/klein-moretti/nix-custompkgs";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
     custompkgs-priv = {
       url = "github:XiaoXioe/nix-custompkg-priv";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -53,6 +48,11 @@
 
     claude-code = {
       url = "github:sadjow/claude-code-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hermes-agent = {
+      url = "github:NousResearch/hermes-agent";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
