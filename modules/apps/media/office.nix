31130e945bea0c33145e0e7ab64dc195deaad2b7
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-06-23 22:50:49 +0700

    feat(office): modularize office configuration using mkModule and mkApp

--- modules/apps/media/office.nix
+++ modules/apps/media/office.nix
@@ -1,23 +1,95 @@
 {
-  pkgs,
   selfLib,
+  pkgs,
+  lib,
+  config,
   ...
 }:
 
-selfLib.mkModule {
-  name = "apps.media.office";
-  description = "Office Apps for users";
-
-  hmConfig = {
-    home.packages = with pkgs; [
-      zathura
-      libreoffice
-    ];
-    programs.onlyoffice = {
-      enable = true;
-      settings = {
-        UITheme = "theme-contrast-dark";
+{
+  imports = [
+    (selfLib.mkModule {
+      name = "apps.media.office";
+      description = "Office applications bundle";
+      options = {
+        flatpak = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Whether to use Flatpak for all office applications by default.";
+          };
+        };
       };
-    };
-  };
+    })
+
+    (selfLib.mkApp {
+      name = "apps.media.zathura";
+      description = "Zathura Document Viewer";
+      options = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = config.my.apps.media.office.enable;
+          description = "Enable Zathura Document Viewer";
+        };
+        flatpak.enable = lib.mkOption {
+          type = lib.types.bool;
+          default = config.my.apps.media.office.flatpak.enable;
+          description = "Whether to use Flatpak for Zathura instead of the native package.";
+        };
+      };
+      flatpak = {
+        appId = "org.pwmt.zathura";
+        symlinks = [
+          {
+            host = ".local/share/zathura";
+            guest = "data/zathura";
+          }
+        ];
+      };
+      native = {
+        package = pkgs.zathura;
+      };
+    })
+
+    (selfLib.mkApp {
+      name = "apps.media.onlyoffice";
+      description = "OnlyOffice Desktop Editors";
+      options = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = config.my.apps.media.office.enable;
+          description = "Enable OnlyOffice";
+        };
+        flatpak.enable = lib.mkOption {
+          type = lib.types.bool;
+          default = config.my.apps.media.office.flatpak.enable;
+          description = "Whether to use Flatpak for OnlyOffice instead of the native package.";
+        };
+      };
+      flatpak = {
+        appId = "org.onlyoffice.desktopeditors";
+        symlinks = [
+          {
+            host = ".config/onlyoffice";
+            guest = "config/onlyoffice";
+          }
+          {
+            host = ".local/share/onlyoffice";
+            guest = "data/onlyoffice";
+          }
+        ];
+      };
+      native = {
+        package = pkgs.onlyoffice-desktopeditors;
+      };
+      hmProgram = {
+        name = "onlyoffice";
+        extraConfig = {
+          settings = {
+            UITheme = "theme-contrast-dark";
+          };
+        };
+      };
+    })
+  ];
 }
