1e84a2c9c9fab676f74cc5a483091a9879fb560a
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-06-23 15:54:54 +0700

    security: apply sysctl kernel hardening parameters

--- modules/security/hardening.nix
+++ modules/security/hardening.nix
@@ -8,6 +8,14 @@ selfLib.mkModule {
   name = "security.hardening";
 
   nixosConfig = {
+    boot.kernel.sysctl = {
+      "kernel.dmesg_restrict" = 1;
+      "kernel.kptr_restrict" = 2;
+      "kernel.unprivileged_bpf_disabled" = 1;
+      "net.ipv4.conf.all.rp_filter" = 1;
+      "net.ipv4.conf.default.rp_filter" = 1;
+    };
+
     environment.systemPackages = [
       pkgs.doas-sudo-shim
     ];
