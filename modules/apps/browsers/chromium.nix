9466aebc4e5c5cddeec92aabcff568a8021eeb95
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-06-25 10:03:31 +0700

    modules/apps/browsers/chromium: Refactor to use flatpakCfg inside mkModule

--- modules/apps/browsers/chromium.nix
+++ modules/apps/browsers/chromium.nix
@@ -4,59 +4,59 @@
   ...
 }:
 
-selfLib.mkApp {
+selfLib.mkModule {
   name = "apps.browsers.chromium";
   description = "Chromium browser configuration";
 
-  flatpak = {
-    appId = "org.chromium.Chromium";
+  flatpakCfg = {
+    "org.chromium.Chromium" = {
+      enable = true;
 
-    overrides = {
-      Context = {
-        filesystems = [
-          "/etc/chromium:ro"
-        ];
+      overrides = {
+        Context = {
+          filesystems = [
+            "/etc/chromium:ro"
+          ];
+        };
       };
-    };
 
-    symlinks = [
-      {
-        host = ".config/chromium";
-        guest = "config/chromium";
-      }
-    ];
-
-    flags = {
-      file = "config/chromium-flags.conf";
-      text = ''
-        --password-store=gnome-libsecret
-        --enable-gpu-rasterization
-        --ignore-gpu-blocklist
-        --disable-gpu-driver-bug-workarounds
-      '';
-    };
-  };
-
-  native = {
-    package = pkgs.chromium;
-  };
-
-  hmProgram = {
-    name = "chromium";
-    extraConfig = {
-      extensions = [
-        { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # uBlock Origin Lite
-        { id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp"; } # Privacy Badger
-        { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden Password Manager
-        { id = "jplgfhpmjnbigmhklmmbgecoobifkmpa"; } # Proton-vpn
-        { id = "hlepfoohegkhhmjieoechaddaejaokhf"; } # Refined GitHub
-        { id = "lptnjkfjeaemenlipfaaocppmilaeejf"; } # ClearURLs
-        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
-        { id = "jinjaccalgkegednnccohejagnlnfdag"; } # Violentmonkey
-        { id = "omkfmpieigblcllmkgbflkikinpkodlk"; } # Enhanced-h264ify
-        { id = "jhnleheckmknfcgijgkadoemagpecfol"; } # Auto Tab Discard (suspend)
-        { id = "cmpdlhmnmjhihmcfnigoememnffkimlk"; } # Catppuccin Macchiato
+      symlinks = [
+        {
+          host = ".config/chromium";
+          guest = "config/chromium";
+        }
       ];
+
+      flags = {
+        file = "config/chromium-flags.conf";
+        text = ''
+          --password-store=gnome-libsecret
+          --enable-gpu-rasterization
+          --ignore-gpu-blocklist
+          --disable-gpu-driver-bug-workarounds
+        '';
+      };
+
+      nativePkgs = pkgs.chromium;
+
+      hmProgram = {
+        name = "chromium";
+        extraConfig = {
+          extensions = [
+            { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # uBlock Origin Lite
+            { id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp"; } # Privacy Badger
+            { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden Password Manager
+            { id = "jplgfhpmjnbigmhklmmbgecoobifkmpa"; } # Proton-vpn
+            { id = "hlepfoohegkhhmjieoechaddaejaokhf"; } # Refined GitHub
+            { id = "lptnjkfjeaemenlipfaaocppmilaeejf"; } # ClearURLs
+            { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
+            { id = "jinjaccalgkegednnccohejagnlnfdag"; } # Violentmonkey
+            { id = "omkfmpieigblcllmkgbflkikinpkodlk"; } # Enhanced-h264ify
+            { id = "jhnleheckmknfcgijgkadoemagpecfol"; } # Auto Tab Discard (suspend)
+            { id = "cmpdlhmnmjhihmcfnigoememnffkimlk"; } # Catppuccin Macchiato
+          ];
+        };
+      };
     };
   };
 
