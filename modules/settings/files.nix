c2796144a4148b473f3d2622b6015292028f984b
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-06-25 17:02:37 +0700

    fix(files): handle flatpak symlinks via system tmpfiles and prevent shadowing

--- modules/settings/files.nix
+++ modules/settings/files.nix
@@ -1,4 +1,5 @@
 {
+  config,
   selfLib,
   ...
 }:
@@ -7,32 +8,46 @@ selfLib.mkModule {
   name = "settings.files";
   description = "Home file settings";
 
-  hmConfig = { config, lib, ... }: {
-    home.activation.setupFlatpakSymlinks = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      # Buat folder target di host jika belum ada
-      mkdir -p /mnt/data_btrfs/flatpak-userdata
-      mkdir -p /mnt/data_btrfs/flatpak-local
-      mkdir -p /mnt/data_btrfs/containers
+  nixosConfig = {
+    systemd.tmpfiles.rules = [
+      "d /mnt/data_btrfs/flatpak-userdata 0755 ${config.my.user.name} users - -"
+      "d /mnt/data_btrfs/flatpak-local 0755 ${config.my.user.name} users - -"
+      "d /mnt/data_btrfs/containers 0755 ${config.my.user.name} users - -"
+    ];
+  };
 
+  hmConfig = hmOpts: {
+    home.activation.setupFlatpakSymlinks = hmOpts.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       # Pastikan direktori penampung di home ada
       mkdir -p "$HOME/.var"
       mkdir -p "$HOME/.local/share"
 
       # Buat symlink langsung (ln -sfn) dari home ke target host
+      # Hapus folder target asli jika ada untuk mencegah nested symlink
+      if [ -d "$HOME/.var/app" ] && [ ! -L "$HOME/.var/app" ]; then
+        rm -rf "$HOME/.var/app"
+      fi
+      if [ -d "$HOME/.local/share/flatpak" ] && [ ! -L "$HOME/.local/share/flatpak" ]; then
+        rm -rf "$HOME/.local/share/flatpak"
+      fi
+      if [ -d "$HOME/.local/share/containers" ] && [ ! -L "$HOME/.local/share/containers" ]; then
+        rm -rf "$HOME/.local/share/containers"
+      fi
+
       ln -sfn /mnt/data_btrfs/flatpak-userdata "$HOME/.var/app"
       ln -sfn /mnt/data_btrfs/flatpak-local "$HOME/.local/share/flatpak"
       ln -sfn /mnt/data_btrfs/containers "$HOME/.local/share/containers"
     '';
 
-    home.file."Documents".source = config.lib.file.mkOutOfStoreSymlink "/mnt/data/Documents";
-    home.file."Downloads".source = config.lib.file.mkOutOfStoreSymlink "/mnt/data/Downloads";
-    home.file."Pictures".source = config.lib.file.mkOutOfStoreSymlink "/mnt/data/Pictures";
-    home.file."Videos".source = config.lib.file.mkOutOfStoreSymlink "/mnt/data/Videos";
-    home.file."Music".source = config.lib.file.mkOutOfStoreSymlink "/mnt/data/Music";
+    home.file."Documents".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/mnt/data/Documents";
+    home.file."Downloads".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/mnt/data/Downloads";
+    home.file."Pictures".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/mnt/data/Pictures";
+    home.file."Videos".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/mnt/data/Videos";
+    home.file."Music".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/mnt/data/Music";
 
     home.file = {
-      ".face.icon".source = config.lib.file.mkOutOfStoreSymlink "/run/secrets/foto-profile";
-      ".face".source = config.lib.file.mkOutOfStoreSymlink "/run/secrets/foto-profile";
+      ".face.icon".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/run/secrets/foto-profile";
+      ".face".source = hmOpts.config.lib.file.mkOutOfStoreSymlink "/run/secrets/foto-profile";
     };
   };
 }
