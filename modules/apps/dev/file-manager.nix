a35675f62699d33ae69aaaef1192b7663a8d972a
Klein Moretti <169626976+XiaoXioe@users.noreply.github.com>
2026-06-27 17:31:57 +0700

    file-manager: customize desktop settings and applications menu

--- modules/apps/dev/file-manager.nix
+++ modules/apps/dev/file-manager.nix
@@ -9,7 +9,7 @@
 let
   defaultApps = {
     text = [ "codium.desktop" ];
-    image = [ "org.gnome.gThumb" ];
+    image = [ "org.gnome.gThumb.desktop" ];
     audio = [ "mpv.desktop" ];
     video = [ "mpv.desktop" ];
     directory = [ "org.kde.dolphin.desktop" ];
@@ -119,127 +119,128 @@ selfLib.mkModule {
   name = "apps.dev.file-manager";
   description = "FileManager user settings";
 
-  hmConfig = hmOpts:
+  hmConfig =
+    hmOpts:
     let
       lib = hmOpts.lib;
     in
     {
-    xdg = {
-      portal = {
-        enable = true;
-        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-        config.common.default = "*";
+      xdg = {
+        portal = {
+          enable = true;
+          extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+          config.common.default = "*";
+        };
+
+        configFile."baloofilerc".text = ''
+          [Basic Settings]
+          Indexing-Enabled=false
+        '';
+
+        configFile."menus/applications.menu".text = ''
+          <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN" "http://www.freedesktop.org/standards/menu-spec/1.0/menu.dtd">
+          <Menu>
+            <Name>Applications</Name>
+            <DefaultAppDirs/>
+            <DefaultDirectoryDirs/>
+            <DefaultMergeDirs/>
+          </Menu>
+        '';
+
+        configFile."dolphinrc".text = ''
+          [DetailsMode]
+          PreviewSize=22
+
+          [General]
+          GlobalViewProps=true
+          Version=202
+
+          [KFileDialog Settings]
+          Places Icons Auto-resize=false
+          Places Icons Static Size=22
+
+          [MainWindow]
+          MenuBar=Disabled
+        '';
+
+        mimeApps = {
+          enable = true;
+          associations.added = associations;
+          defaultApplications = associations;
+        };
       };
 
-      configFile."baloofilerc".text = ''
-        [Basic Settings]
-        Indexing-Enabled=false
-      '';
+      home = {
+        activation.updateKdeCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          run env XDG_DATA_DIRS="/run/current-system/sw/share:/etc/profiles/per-user/${userName}/share:$XDG_DATA_DIRS" ${pkgs.kdePackages.kservice}/bin/kbuildsycoca6 --noincremental
+        '';
+        sessionVariables = {
+          XDG_DATA_DIRS = "/run/current-system/sw/share:/etc/profiles/per-user/${userName}/share:$XDG_DATA_DIRS";
+          QT_STYLE_OVERRIDE = "breeze";
+          XCURSOR_THEME = "breeze_cursors";
+        };
 
-      configFile."menus/applications.menu".text = ''
-        <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN" "http://www.freedesktop.org/standards/menu-spec/1.0/menu.dtd">
-        <Menu>
-          <Name>Applications</Name>
-          <DefaultAppDirs/>
-          <DefaultDirectoryDirs/>
-          <DefaultMergeDirs/>
-        </Menu>
-      '';
+        packages = with pkgs; [
+          kdePackages.dolphin
+          kdePackages.kio
+          kdePackages.kio-extras
+          kdePackages.kservice
+          kdePackages.breeze-icons
+          kdePackages.qqc2-desktop-style
 
-      configFile."dolphinrc".text = ''
-        [DetailsMode]
-        PreviewSize=22
+          zip
+          unzip
+          # rar
+          unrar
+          p7zip
 
-        [General]
-        GlobalViewProps=true
-        Version=202
+          # nemo-with-extensions
+          # nemo-fileroller
+        ];
+      };
 
-        [KFileDialog Settings]
-        Places Icons Auto-resize=false
-        Places Icons Static Size=22
-
-        [MainWindow]
-        MenuBar=Disabled
-      '';
-
-      mimeApps = {
-        enable = true;
-        associations.added = associations;
-        defaultApplications = associations;
+      dconf.settings = {
+        "org/nemo/preferences" = {
+          always-use-browser = true;
+          close-device-view-on-device-eject = true;
+          date-font-choice = "auto-mono";
+          date-format = "iso";
+          last-server-connect-method = 3;
+          default-folder-viewer = "list-view";
+          inherit-folder-viewer = true;
+          quick-renames-with-pause-in-between = true;
+          show-edit-icon-toolbar = false;
+          show-full-path-titles = false;
+          show-hidden-files = true;
+          show-home-icon-toolbar = true;
+          show-new-folder-icon-toolbar = true;
+          show-open-in-terminal-toolbar = false;
+          show-search-icon-toolbar = false;
+          show-show-thumbnails-toolbar = false;
+          thumbnail-limit = lib.gvariant.mkUint64 (100 * 1024 * 1024);
+        };
+        "org/nemo/list-view" = {
+          default-zoom-level = "smaller";
+          enable-folder-expansion = true;
+        };
+        "org/nemo/preferences/menu-config" = {
+          background-menu-open-as-root = false;
+          selection-menu-open-as-root = false;
+          selection-menu-open-in-terminal = false;
+          selection-menu-scripts = false;
+        };
+        "org/nemo/search" = {
+          search-reverse-sort = false;
+          search-sort-column = "name";
+        };
+        "org/nemo/window-state" = {
+          maximized = true;
+          network-expanded = true;
+          side-pane-view = "places";
+          sidebar-bookmark-breakpoint = 2;
+          sidebar-width = lib.gvariant.mkInt32 180;
+          start-with-sidebar = true;
+        };
       };
     };
-
-    home = {
-      activation.updateKdeCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        run env XDG_DATA_DIRS="/run/current-system/sw/share:/etc/profiles/per-user/${userName}/share:$XDG_DATA_DIRS" ${pkgs.kdePackages.kservice}/bin/kbuildsycoca6 --noincremental
-      '';
-      sessionVariables = {
-        XDG_DATA_DIRS = "/run/current-system/sw/share:/etc/profiles/per-user/${userName}/share:$XDG_DATA_DIRS";
-        QT_STYLE_OVERRIDE = "breeze";
-        XCURSOR_THEME = "breeze_cursors";
-      };
-
-      packages = with pkgs; [
-        kdePackages.dolphin
-        kdePackages.kio
-        kdePackages.kio-extras
-        kdePackages.kservice
-        kdePackages.breeze-icons
-        kdePackages.qqc2-desktop-style
-
-        zip
-        unzip
-        # rar
-        unrar
-        p7zip
-
-        # nemo-with-extensions
-        # nemo-fileroller
-      ];
-    };
-
-    dconf.settings = {
-      "org/nemo/preferences" = {
-        always-use-browser = true;
-        close-device-view-on-device-eject = true;
-        date-font-choice = "auto-mono";
-        date-format = "iso";
-        last-server-connect-method = 3;
-        default-folder-viewer = "list-view";
-        inherit-folder-viewer = true;
-        quick-renames-with-pause-in-between = true;
-        show-edit-icon-toolbar = false;
-        show-full-path-titles = false;
-        show-hidden-files = true;
-        show-home-icon-toolbar = true;
-        show-new-folder-icon-toolbar = true;
-        show-open-in-terminal-toolbar = false;
-        show-search-icon-toolbar = false;
-        show-show-thumbnails-toolbar = false;
-        thumbnail-limit = lib.gvariant.mkUint64 (100 * 1024 * 1024);
-      };
-      "org/nemo/list-view" = {
-        default-zoom-level = "smaller";
-        enable-folder-expansion = true;
-      };
-      "org/nemo/preferences/menu-config" = {
-        background-menu-open-as-root = false;
-        selection-menu-open-as-root = false;
-        selection-menu-open-in-terminal = false;
-        selection-menu-scripts = false;
-      };
-      "org/nemo/search" = {
-        search-reverse-sort = false;
-        search-sort-column = "name";
-      };
-      "org/nemo/window-state" = {
-        maximized = true;
-        network-expanded = true;
-        side-pane-view = "places";
-        sidebar-bookmark-breakpoint = 2;
-        sidebar-width = lib.gvariant.mkInt32 180;
-        start-with-sidebar = true;
-      };
-    };
-  };
 }
