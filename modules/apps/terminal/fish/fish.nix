c39a61667f9ba8adab3119956e544418052513d8
Klein Moretti <klein.moretti@localhost>
2026-07-03 22:28:19 +0700

    fish: simplify nfu alias to nix flake update

--- modules/apps/terminal/fish/fish.nix
+++ modules/apps/terminal/fish/fish.nix
@@ -94,7 +94,7 @@
         # --- NIXOS REBUILD & MAINTENANCE ---
         rebuild = "sudo nixos-rebuild switch --flake --print-build-logs --show-trace";
         cln = "nh clean all --keep 3 --ask --optimise";
-        nfu = "nix flake update --flake";
+        nfu = "nix flake update";
         osbuild = "nh os switch --no-nom --show-trace --diff auto --ask -L";
         ostest = "nh os test --no-nom --show-trace --diff auto --ask -L";
         osboot = "nh os boot --no-nom --show-trace --diff auto --ask -L";
