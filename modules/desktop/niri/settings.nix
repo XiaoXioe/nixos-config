80db09154d709184321f637f932fe5cc1b6d1aa3
Klein Moretti <klein.moretti@localhost>
2026-07-04 17:55:42 +0700

    desktop/niri: nixfmt formatting

--- modules/desktop/niri/settings.nix
+++ modules/desktop/niri/settings.nix
@@ -1,5 +1,6 @@
 { config, osConfig, ... }:
 {
   # Link niri configuration from the repository using out-of-store symlink
-  xdg.configFile."niri".source = config.lib.file.mkOutOfStoreSymlink "${osConfig.my.user.flakePath}/dotfiles/niri";
+  xdg.configFile."niri".source =
+    config.lib.file.mkOutOfStoreSymlink "${osConfig.my.user.flakePath}/dotfiles/niri";
 }
