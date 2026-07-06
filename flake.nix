6a18e7264585ef5db54a3dad8c6fec970d63dc9e
Klein Moretti <klein.moretti@localhost>
2026-07-06 09:38:01 +0700

    refactor(flake): remove commented-out local custompkgs override

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # custompkgs = {
-    #   url = "path:/home/klein-moretti/nix-custompkgs";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
     custompkgs-priv = {
       url = "github:XiaoXioe/nix-custompkg-priv";
       inputs.nixpkgs.follows = "nixpkgs";
