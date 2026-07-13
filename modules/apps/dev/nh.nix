433438d6eda570fc36c276137964a1e02b53820b
Klein Moretti <klein.moretti@localhost>
2026-07-13 09:57:39 +0700

    feat(nh): enable weekly auto cleanup keeping 3 generations

--- modules/apps/dev/nh.nix
+++ modules/apps/dev/nh.nix
@@ -8,6 +8,17 @@ selfLib.mkModule {
   name = "apps.dev.nh";
   description = "NH configuration";
 
+  nixosConfig = {
+    programs.nh = {
+      enable = true;
+      clean = {
+        enable = true;
+        dates = "weekly";
+        extraArgs = "--keep 3";
+      };
+    };
+  };
+
   hmConfig = hmOpts: {
     programs.nh = {
       enable = true;
