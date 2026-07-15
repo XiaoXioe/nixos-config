bb9e16c6433b2388ed88fe105a978f91d8b97154
Klein Moretti <klein.moretti@localhost>
2026-07-15 20:25:48 +0700

    feat(ai): add ggml-org/llama.cpp to inputs

--- flake.nix
+++ flake.nix
@@ -67,6 +67,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    llama-cpp = {
+      url = "github:ggml-org/llama.cpp";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs =
