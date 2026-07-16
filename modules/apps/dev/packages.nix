00c26cc603173c0fec6c2513159ea32a30238539
Klein Moretti <klein.moretti@localhost>
2026-07-16 14:01:48 +0700

    feat(packages): declaratively update kaggle cli to v2.2.3 via local package overrides

--- modules/apps/dev/packages.nix
+++ modules/apps/dev/packages.nix
@@ -8,15 +8,55 @@ selfLib.mkModule {
   name = "apps.dev.packages";
   description = "Packages for development";
 
-  hmConfig = hmOpts: {
-    home.packages = with pkgs; [
-      nodejs_22
-      uv
-      nix-tree
-      nix-init
-      python3
-      cachix
-      kaggle
-    ];
-  };
+  hmConfig =
+    hmOpts:
+    let
+      myPython = pkgs.python3.override {
+        packageOverrides = self: super: {
+          kagglesdk = super.kagglesdk.overrideAttrs (oldAttrs: rec {
+            version = "0.1.34";
+            src = pkgs.fetchPypi {
+              pname = "kagglesdk";
+              inherit version;
+              hash = "sha256-DAp6Gp20d77k+hlOQR6A/Y2TunOXfaQAF1Ui8c+S2lg=";
+            };
+          });
+
+          kaggle = super.kaggle.overrideAttrs (oldAttrs: rec {
+            version = "2.2.3";
+            src = pkgs.fetchPypi {
+              pname = "kaggle";
+              inherit version;
+              hash = "sha256-ziksRZoTlQVT2JTuc2jWRUvlMpG9dcLSetK+0UnN1sQ=";
+            };
+            propagatedBuildInputs = with self; [
+              bleach
+              kagglesdk
+              python-slugify
+              requests
+              python-dateutil
+              tqdm
+              urllib3
+              packaging
+              protobuf
+              jupytext
+              python-dotenv
+            ];
+            nativeBuildInputs = [ self.hatchling ] ++ (oldAttrs.nativeBuildInputs or [ ]);
+          });
+        };
+      };
+      kaggle-app = myPython.pkgs.toPythonApplication myPython.pkgs.kaggle;
+    in
+    {
+      home.packages = with pkgs; [
+        nodejs_22
+        uv
+        nix-tree
+        nix-init
+        python3
+        cachix
+        kaggle-app
+      ];
+    };
 }
