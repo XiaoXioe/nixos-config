7347f0e4fdb459113c86f5502354d29c211d6469
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-07-19 09:28:55 +0700

    feat(vscodium): add skipNativeWrapper

--- modules/apps/editors/vscodium.nix
+++ modules/apps/editors/vscodium.nix
@@ -133,178 +133,184 @@ selfLib.mkModule {
         }
       ];
       nativePkgs = pkgs.vscodium;
+      skipNativeWrapper = true;
     };
   };
 
-  hmConfig = hmOpts: {
-    home.packages = with pkgs; [
-      black
-      shfmt
-      nixfmt
-      ruff
-      shellcheck
-      nixd
-      sqlite
-      sqlfluff
-      php
-      clang-tools
-    ];
-    programs.vscodium = {
-      enable = true;
-      package = pkgs.lib.makeOverridable (
-        args:
-        pkgs.writeShellScriptBin "codium" ''
-          exec flatpak run com.vscodium.codium "$@"
-        ''
-      ) { };
-      argvSettings = {
-        ignore-gpu-blocklist = true;
-        enable-crash-reporter = false;
-        disable-gpu-compositing = false;
-      };
-      mutableExtensionsDir = false;
-      profiles.default = {
-        extensions = builtinExts ++ marketplaceExts;
-        userSettings = {
-          ## UI
-          "workbench.colorTheme" = "Catppuccin Mocha";
-          "workbench.iconTheme" = "catppuccin-mocha";
-          "editor.fontFamily" = "'Adwaita Mono', 'JetBrainsMono Nerd Font', monospace";
-          "editor.fontSize" = 18;
-          "editor.fontLigatures" = true;
-          "editor.minimap.enabled" = false;
+  hmConfig =
+    hmOpts:
+    let
+      lib = hmOpts.lib;
+    in
+    {
+      home.packages = with pkgs; [
+        black
+        shfmt
+        nixfmt
+        ruff
+        shellcheck
+        nixd
+        sqlite
+        sqlfluff
+        php
+        clang-tools
+      ];
+      programs.vscodium = {
+        enable = true;
+        package = lib.makeOverridable (
+          args:
+          pkgs.writeShellScriptBin "codium" ''
+            exec flatpak run com.vscodium.codium "$@"
+          ''
+        ) { };
+        argvSettings = {
+          ignore-gpu-blocklist = true;
+          enable-crash-reporter = false;
+          disable-gpu-compositing = false;
+        };
+        mutableExtensionsDir = false;
+        profiles.default = {
+          extensions = builtinExts ++ marketplaceExts;
+          userSettings = {
+            ## UI
+            "workbench.colorTheme" = "Catppuccin Mocha";
+            "workbench.iconTheme" = "catppuccin-mocha";
+            "editor.fontFamily" = "'Adwaita Mono', 'JetBrainsMono Nerd Font', monospace";
+            "editor.fontSize" = 18;
+            "editor.fontLigatures" = true;
+            "editor.minimap.enabled" = false;
 
-          # Layout
-          "workbench.startupEditor" = "none";
-          "workbench.editor.showTabs" = "none";
-          "workbench.editor.limit.enabled" = true;
-          "workbench.editor.limit.value" = 10;
-          "workbench.editor.limit.perEditorGroup" = true;
-          "workbench.sideBar.location" = "right";
-          "workbench.activityBar.location" = "hidden";
-          "workbench.statusBar.visible" = false;
-          "workbench.layoutControl.enabled" = false;
-          "explorer.openEditors.visible" = 0;
-          "breadcrumbs.enabled" = false;
-          "editor.scrollbar.verticalScrollbarSize" = 2;
-          "editor.scrollbar.horizontalScrollbarSize" = 2;
-          "editor.scrollbar.vertical" = "hidden";
-          "editor.scrollbar.horizontal" = "hidden";
+            # Layout
+            "workbench.startupEditor" = "none";
+            "workbench.editor.showTabs" = "none";
+            "workbench.editor.limit.enabled" = true;
+            "workbench.editor.limit.value" = 10;
+            "workbench.editor.limit.perEditorGroup" = true;
+            "workbench.sideBar.location" = "right";
+            "workbench.activityBar.location" = "hidden";
+            "workbench.statusBar.visible" = false;
+            "workbench.layoutControl.enabled" = false;
+            "explorer.openEditors.visible" = 0;
+            "breadcrumbs.enabled" = false;
+            "editor.scrollbar.verticalScrollbarSize" = 2;
+            "editor.scrollbar.horizontalScrollbarSize" = 2;
+            "editor.scrollbar.vertical" = "hidden";
+            "editor.scrollbar.horizontal" = "hidden";
 
-          # Window
-          "window.titleBarStyle" = "custom";
-          "window.menuBarVisibility" = "toggle";
-          "editor.mouseWheelZoom" = true;
+            # Window
+            "window.titleBarStyle" = "custom";
+            "window.menuBarVisibility" = "toggle";
+            "editor.mouseWheelZoom" = true;
 
-          # Files
-          "files.autoSave" = "onWindowChange";
-          "files.insertFinalNewline" = true;
-          "explorer.confirmDragAndDrop" = false;
-          "editor.renderControlCharacters" = false;
+            # Files
+            "files.autoSave" = "onWindowChange";
+            "files.insertFinalNewline" = true;
+            "explorer.confirmDragAndDrop" = false;
+            "editor.renderControlCharacters" = false;
 
-          # Format on save
-          "editor.formatOnSave" = true;
-          "editor.formatOnType" = true;
-          "editor.formatOnPaste" = true;
-          "editor.inlayHints.enabled" = "off";
+            # Format on save
+            "editor.formatOnSave" = true;
+            "editor.formatOnType" = true;
+            "editor.formatOnPaste" = true;
+            "editor.inlayHints.enabled" = "off";
 
-          # Updates
-          "update.mode" = "none";
-          "extensions.autoUpdate" = false;
-          "vsicons.dontShowNewVersionMessage" = true;
+            # Updates
+            "update.mode" = "none";
+            "extensions.autoUpdate" = false;
+            "vsicons.dontShowNewVersionMessage" = true;
 
-          "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
-          "nix.enableLanguageServer" = true;
-          "nix.formatterPath" = "${pkgs.nixfmt}/bin/nixfmt";
-          "nix.serverSettings" = {
-            "nixd" = {
-              "formatting" = {
-                "command" = [ "${pkgs.nixfmt}/bin/nixfmt" ];
+            "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+            "nix.enableLanguageServer" = true;
+            "nix.formatterPath" = "${pkgs.nixfmt}/bin/nixfmt";
+            "nix.serverSettings" = {
+              "nixd" = {
+                "formatting" = {
+                  "command" = [ "${pkgs.nixfmt}/bin/nixfmt" ];
+                };
               };
             };
-          };
 
-          # Per-language formatters
-          "[python]" = {
-            "editor.defaultFormatter" = "ms-python.black-formatter";
-            "editor.codeActionsOnSave" = {
-              "source.fixAll" = "explicit";
-              "source.organizeImports" = "explicit";
+            # Per-language formatters
+            "[python]" = {
+              "editor.defaultFormatter" = "ms-python.black-formatter";
+              "editor.codeActionsOnSave" = {
+                "source.fixAll" = "explicit";
+                "source.organizeImports" = "explicit";
+              };
             };
-          };
-          "[shellscript]" = {
-            "editor.defaultFormatter" = "foxundermoon.shell-format";
-            "editor.codeActionsOnSave.source.fixAll" = "explicit";
-          };
-          "[fish]" = {
-            "editor.defaultFormatter" = "bmalehorn.vscode-fish";
-          };
-          "[toml]" = {
-            "editor.defaultFormatter" = "tamasfe.even-better-toml";
-          };
+            "[shellscript]" = {
+              "editor.defaultFormatter" = "foxundermoon.shell-format";
+              "editor.codeActionsOnSave.source.fixAll" = "explicit";
+            };
+            "[fish]" = {
+              "editor.defaultFormatter" = "bmalehorn.vscode-fish";
+            };
+            "[toml]" = {
+              "editor.defaultFormatter" = "tamasfe.even-better-toml";
+            };
 
-          # --- TAMBAHAN KONFIGURASI SQLFLUFF & PHP ---
+            # --- TAMBAHAN KONFIGURASI SQLFLUFF & PHP ---
 
-          # Konfigurasi SQLFluff (Dialek SQLite)
-          "sqlfluff.dialect" = "sqlite";
-          "sqlfluff.executablePath" = "${pkgs.sqlfluff}/bin/sqlfluff";
-          "sqlfluff.format.enabled" = true;
-          "sqlfluff.linter.run" = "onType";
-          "sqlfluff.linter.diagnosticSeverity" = "error";
-          "sqlfluff.format.arguments" = [ "--FIX-EVEN-UNPARSABLE" ];
+            # Konfigurasi SQLFluff (Dialek SQLite)
+            "sqlfluff.dialect" = "sqlite";
+            "sqlfluff.executablePath" = "${pkgs.sqlfluff}/bin/sqlfluff";
+            "sqlfluff.format.enabled" = true;
+            "sqlfluff.linter.run" = "onType";
+            "sqlfluff.linter.diagnosticSeverity" = "error";
+            "sqlfluff.format.arguments" = [ "--FIX-EVEN-UNPARSABLE" ];
 
-          # Tentukan Formatter Default per Bahasa
-          "[sql]" = {
-            "editor.defaultFormatter" = "sqlfluff.vscode-sqlfluff";
+            # Tentukan Formatter Default per Bahasa
+            "[sql]" = {
+              "editor.defaultFormatter" = "sqlfluff.vscode-sqlfluff";
+            };
+
+            "[php]" = {
+              "editor.defaultFormatter" = "bmewburn.vscode-intelephense-client";
+            };
+
+            "intelephense.format.enable" = true;
+
+            "[yaml]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[javascript]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[html]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[json]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[jsonc]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+
+            # MQL5
+            "[mql5]" = {
+              "editor.defaultFormatter" = "ngSoftware.mql-clangd";
+            };
+
+            # Error Lens
+            "errorLens.delay" = 500;
+            "errorLens.enabledDiagnosticLevels" = [
+              "error"
+              "warning"
+            ];
+            "errorLens.gutterIconsEnabled" = false;
+            "errorLens.fontStyleItalic" = true;
+
+            # Built-in linters
+            "json.validate.enable" = true;
+            "shellcheck.executablePath" = "${pkgs.shellcheck}/bin/shellcheck";
+            "shellformat.path" = "${pkgs.shfmt}/bin/shfmt";
+            "black-formatter.path" = [ "${pkgs.black}/bin/black" ];
+            "ruff.path" = [ "${pkgs.ruff}/bin/ruff" ];
+
+            # Disable workspace trust prompt
+            "security.workspace.trust.enabled" = false;
           };
-
-          "[php]" = {
-            "editor.defaultFormatter" = "bmewburn.vscode-intelephense-client";
-          };
-
-          "intelephense.format.enable" = true;
-
-          "[yaml]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[javascript]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[html]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[json]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[jsonc]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-
-          # MQL5
-          "[mql5]" = {
-            "editor.defaultFormatter" = "ngSoftware.mql-clangd";
-          };
-
-          # Error Lens
-          "errorLens.delay" = 500;
-          "errorLens.enabledDiagnosticLevels" = [
-            "error"
-            "warning"
-          ];
-          "errorLens.gutterIconsEnabled" = false;
-          "errorLens.fontStyleItalic" = true;
-
-          # Built-in linters
-          "json.validate.enable" = true;
-          "shellcheck.executablePath" = "${pkgs.shellcheck}/bin/shellcheck";
-          "shellformat.path" = "${pkgs.shfmt}/bin/shfmt";
-          "black-formatter.path" = [ "${pkgs.black}/bin/black" ];
-          "ruff.path" = [ "${pkgs.ruff}/bin/ruff" ];
-
-          # Disable workspace trust prompt
-          "security.workspace.trust.enabled" = false;
         };
       };
     };
-  };
 }
