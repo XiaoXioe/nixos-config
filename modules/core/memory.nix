0196d08046c612c2dc98763f5336383d103b59a1
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-07-21 22:16:56 +0700

    feat(memory): mount user and root nix caches on tmpfs

--- modules/core/memory.nix
+++ modules/core/memory.nix
@@ -1,4 +1,4 @@
-{ selfLib, ... }:
+{ config, selfLib, ... }:
 selfLib.mkModule {
   name = "core.memory";
   nixosConfig = {
@@ -17,6 +17,34 @@ selfLib.mkModule {
         "vm.watermark_scale_factor" = 125;
         "vm.watermark_boost_factor" = 0;
       };
+    };
+
+    fileSystems."/home/${config.my.user.name}/.cache/nix" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [
+        "rw"
+        "nodev"
+        "nosuid"
+        "size=2G"
+        "mode=0700"
+        "uid=1000"
+        "gid=100"
+      ];
+    };
+
+    fileSystems."/root/.cache/nix" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [
+        "rw"
+        "nodev"
+        "nosuid"
+        "size=1G"
+        "mode=0700"
+        "uid=0"
+        "gid=0"
+      ];
     };
 
     zramSwap = {
