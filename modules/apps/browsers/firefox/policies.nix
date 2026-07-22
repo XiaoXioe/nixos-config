80f0efa451eb6d892f1615760f1ed0c0c20df47c
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-07-22 10:50:18 +0700

    feat(firefox): lock xpinstall.enabled preference to false

--- modules/apps/browsers/firefox/policies.nix
+++ modules/apps/browsers/firefox/policies.nix
@@ -99,6 +99,7 @@
   "dom.disable_window_move_resize" = lock-true; # Blokir situs web mengubah ukuran atau memindahkan jendela browser
   "devtools.debugger.remote-enabled" = lock-false; # Matikan remote debugging demi keamanan lokal
   "extensions.enabledScopes" = lock 5; # Batasi direktori instalasi ekstensi hanya dari profil pengguna (cegah instalasi siluman)
+  "xpinstall.enabled" = lock-false; # Matikan instalasi ekstensi dari web/AMO untuk pengguna
   "app.shield.optoutstudies.enabled" = lock-false; # Matikan partisipasi dalam studi/uji coba Firefox Shield
   "nimbus.rollouts.enabled" = lock-false; # Matikan eksperimen rollout fitur otomatis Mozilla
 }
