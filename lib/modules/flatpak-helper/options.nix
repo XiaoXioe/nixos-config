9cbd2d95bd5ebc2bfeed80c0f1e7f3a0abeac709
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-07-22 08:47:59 +0700

    refactor(flatpak-helper): add option generator for flatpak module definitions

---
+++ lib/modules/flatpak-helper/options.nix
@@ -0,0 +1,55 @@
+{ lib, ... }:
+
+let
+  utils = import ./utils.nix { inherit lib; };
+in
+{
+  mkFlatpakOptions =
+    {
+      name,
+      flatpakCfg,
+      options,
+      config,
+      singleAppInfo,
+    }:
+    let
+      optionPath = utils.splitName name;
+      isSingleApp = singleAppInfo.isSingleApp;
+      singleAppId = singleAppInfo.singleAppId;
+    in
+    lib.optionalAttrs (flatpakCfg != { } && !isSingleApp) {
+      flatpaks = lib.mapAttrs (appId: appVal: {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Whether to enable ${appId}.";
+        };
+        flatpak = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default =
+              if (options ? flatpak && options.flatpak ? enable) then
+                lib.getAttrFromPath (
+                  optionPath
+                  ++ [
+                    "flatpak"
+                    "enable"
+                  ]
+                ) config.my
+              else
+                appVal.flatpak or true;
+            description = "Whether to use Flatpak for ${appId} instead of the native package.";
+          };
+        };
+      }) flatpakCfg;
+    }
+    // lib.optionalAttrs (flatpakCfg != { } && isSingleApp) {
+      flatpak = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = flatpakCfg.${singleAppId}.flatpak or true;
+          description = "Whether to use Flatpak for ${singleAppId} instead of the native package.";
+        };
+      };
+    };
+}
