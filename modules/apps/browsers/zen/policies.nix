b5e7df67f2195afec746452983c44caf2ea3d111
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-07-22 10:50:19 +0700

    feat(zen): lock xpinstall.enabled preference to false

--- modules/apps/browsers/zen/policies.nix
+++ modules/apps/browsers/zen/policies.nix
@@ -48,6 +48,7 @@
   "browser.startup.page" = lock 3; # Restore session
   "browser.shell.checkDefaultBrowser" = lock-false;
   "extensions.autoDisableScopes" = lock 0;
+  "xpinstall.enabled" = lock-false;
   "zen.welcome-screen.seen" = lock-true;
   "zen.workspaces.continue-where-left-off" = lock-true;
   "browser.aboutwelcome.enabled" = lock-false;
