edc42b378d01eb75cc4a2afdbdd01a7ea30a1694
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-08-05 15:06:01 +0700

    chore(flake): sync inputs and lockfile

--- flake.nix
+++ flake.nix
@@ -10,11 +10,13 @@
 
     custompkgs = {
       url = "github:XiaoXioe/nix-custompkgs";
+      # url = "path:/home/klein-moretti/nix-custompkgs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     custompkgs-priv = {
       url = "github:XiaoXioe/nix-custompkg-priv";
+      # url = "path:/home/klein-moretti/nix-custompkg-priv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
