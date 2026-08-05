cc252fa12a01e0860b92cb63b36ecead32190898
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-08-05 18:12:31 +0700

    fix(media): update mpv, yt-dlp, and font configurations for YouTube dubbing and streaming

--- modules/apps/media/video/yt-dlp.nix
+++ modules/apps/media/video/yt-dlp.nix
@@ -7,28 +7,36 @@ selfLib.mkModule {
   name = "apps.media.video.yt-dlp";
   description = "yt-dlp CLI video downloader configuration";
 
-  hmConfig = hmOpts: {
-    programs.yt-dlp = {
-      enable = true;
-      settings = {
-        format = "'bv+ba/b'";
-        merge-output-format = "mkv";
-        output = "'%(title)s [%(id)s].%(ext)s'";
+  hmConfig =
+    hmOpts:
+    let
+      homeDir = hmOpts.config.home.homeDirectory;
+      username = hmOpts.config.home.username;
+      zenProfile = "${homeDir}/.config/zen/${username}";
+    in
+    {
+      programs.yt-dlp = {
+        enable = true;
+        settings = {
+          format = "'bv+ba/b'";
+          merge-output-format = "mkv";
+          output = "'%(title)s [%(id)s].%(ext)s'";
 
-        cookies-from-browser = "firefox";
+          cookies-from-browser = "firefox:${zenProfile}";
 
-        embed-metadata = true;
-        embed-thumbnail = true;
-        embed-chapters = true;
+          embed-metadata = true;
+          embed-thumbnail = true;
+          embed-chapters = true;
 
-        embed-subs = true;
-        sub-langs = "en,id,-live_chat";
+          embed-subs = true;
+          sub-langs = "en,id,-live_chat";
 
-        concurrent-fragments = 4;
-        no-mtime = true;
-        sponsorblock-mark = "all";
-        sponsorblock-remove = "sponsor";
+          concurrent-fragments = 4;
+          force-ipv4 = true;
+          no-mtime = true;
+          sponsorblock-mark = "all";
+          sponsorblock-remove = "sponsor";
+        };
       };
     };
-  };
 }
