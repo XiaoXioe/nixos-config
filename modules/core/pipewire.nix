ce2235aaaf1612db1a81661fe395fed8836fcc3d
XiaoXioe <169626976+XiaoXioe@users.noreply.github.com>
2026-08-15 08:17:59 +0700

    fix(audio/pipewire): disable power saving and suspend to prevent popping and delays

--- modules/core/pipewire.nix
+++ modules/core/pipewire.nix
@@ -9,11 +9,32 @@ selfLib.mkModule {
 
   nixosConfig = {
 
+    boot.extraModprobeConfig = ''
+      options snd_hda_intel power_save=0 power_save_controller=N
+      options snd_hda_codec_realtek power_save=0
+    '';
+
     services.pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+
+      wireplumber.extraConfig."99-disable-suspend" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              { "node.name" = "~alsa_input.*"; }
+              { "node.name" = "~alsa_output.*"; }
+            ];
+            actions = {
+              update-props = {
+                "session.suspend-timeout-seconds" = 0;
+              };
+            };
+          }
+        ];
+      };
     };
 
     environment.sessionVariables = {
